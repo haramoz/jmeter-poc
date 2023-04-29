014786608d3263f715752b16c6eeb697687302c8
Arka Mallick <arkamallick30@gmail.com>
2023-04-29 17:12:07 +0200

    adding health check in docker compose, still does not work

--- src/db/init-mongo.js
+++ src/db/init-mongo.js
@@ -3,38 +3,23 @@ db.auth('admin', 'admin')
 db = db.getSiblingDB('mydb')
 
 db.createUser({
-  user: 'myuser',
-  pwd: 'mypassword',
-  roles: [
-    {
-      role: 'dbOwner',
-      db: 'mydb',
-    },
-  ],
+    user: 'myuser',
+    pwd: 'mypassword',
+    roles: [
+        {
+            role: 'dbOwner',
+            db: 'mydb',
+        },
+    ],
 });
 
-db.createCollection('orders');
-db.createCollection('menu');
+db.createCollection('device');
 
-db.menu.insert([
-  {
-    item: 'Spicy Tacos',
-    description: 'Tacos with a kick of spice',
-    ingredients: ['pork', 'lettuce', 'tomatoes', 'cheese', 'spicy salsa'],
-    spicyLevel: 'hot'
-  },
-  {
-    item: 'Margherita Pizza',
-    description: 'Classic Italian pizza with fresh mozzarella and basil',
-    ingredients: ['mozzarella cheese', 'tomato sauce', 'fresh basil', 'olive oil'],
-    spicyLevel: 'mild'
-  },
-  {
-    item: 'Spicy Chicken Curry',
-    description: 'Creamy chicken curry with a spicy kick',
-    ingredients: ['chicken', 'coconut milk', 'curry powder', 'chili peppers', 'onions', 'garlic'],
-    spicyLevel: 'very hot'
-  }
+db.device.insert([
+    {
+        name: 'OLT 1',
+        description: 'OLT from our lab',
+        properties: [],
+        type: 'openolt'
+    }
 ]);
-
-
